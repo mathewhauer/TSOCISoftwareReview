f79b034eabfb9f2c7c8fea74cb996899546af396
Matt Hauer <16724401+mathewhauer@users.noreply.github.com>
2022-12-01 12:00:33 -0500

    Blinded.

--- MainDocument/SCRIPTS/000-Libraries.R
+++ MainDocument/SCRIPTS/000-Libraries.R
@@ -61,7 +61,6 @@ pkgs <- c(
   "bsts",
   "CausalImpact",
   "kableExtra",
-  "lettercase",
   "philentropy",
   "ggridges",
   "janitor",
@@ -85,10 +84,7 @@ pkgs <- c(
 # Will only run if at least one package is missing
 
 if(!sum(!p_isinstalled(pkgs))==0){
-  p_install(
-    package = pkgs[!p_isinstalled(pkgs)], 
-    character.only = TRUE
-  )
+  install.packages( pkgs[!p_isinstalled(pkgs)])
 }
 
 # load the packages
@@ -104,9 +100,3 @@ getDoParWorkers() # Determine Number of Utilized Clusters
 getDoParName() #  Name of the Currently Registered Parallel Computing Backend
 getDoParVersion() #  Version of the Currently Registered Parallel Computing Backend
 
-write.excel <- function(x,row.names=FALSE,col.names=TRUE,...) {
-  write.table(x,"clipboard",sep="\t",row.names=row.names,col.names=col.names,...)
-}
-
-arima_order <- c(0,1,1) # setting the global arima model
-arma <- "ARIMA(0,1,1)"
